2c8a8f463af9a7ae152abae7141dd640e300d2e1
lambday <heavensdevil6909@gmail.com>
2016-07-14 03:31:16 +0100

    fixed input parser segfault bug for streaming generators

--- src/shogun/io/streaming/InputParser.h
+++ src/shogun/io/streaming/InputParser.h
@@ -442,7 +442,8 @@ template <class T>
     }
 
     SG_SDEBUG("creating parse thread\n")
-    examples_ring->init_vector();
+    if (examples_ring)
+		examples_ring->init_vector();
     pthread_create(&parse_thread, NULL, parse_loop_entry_point, this);
 
     SG_SDEBUG("leaving CInputParser::start_parser()\n")
